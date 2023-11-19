e7e7d77a482ef9fcd991de12c5024a5abedd4d6e
Lawanya Veer <112944303+lawanyaveer@users.noreply.github.com>
2023-11-19 21:59:29 +0530

    Updated Solution.java

--- Day 2/Solution.java
+++ Day 2/Solution.java
@@ -1,1 +1,27 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int k = 0;
 
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 2, 3, 4, 5, 3, 6};
+        int val = 3;
+
+        int k = new Solution().removeElement(nums, val);
+
+        System.out.println("New length: " + k);
+        System.out.print("Modified array: ");
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
