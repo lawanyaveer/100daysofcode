76adac59324083104a454079290037bd864e518a
Lawanya Veer <112944303+lawanyaveer@users.noreply.github.com>
2023-11-27 23:53:05 +0530

    Updated Solution.py

--- Day 5/Solution.py
+++ Day 5/Solution.py
@@ -2,7 +2,6 @@ class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         candidate = None
         count = 0
-
         for num in nums:
             if count == 0:
                 candidate = num
