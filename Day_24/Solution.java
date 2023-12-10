fe1e3b1c7c51ed030e2ebd3e5462e25250536915
Lawanya Veer <112944303+lawanyaveer@users.noreply.github.com>
2023-12-11 02:48:14 +0530

    Updated Solution.java

--- Day_24/Solution.java
+++ Day_24/Solution.java
@@ -1,20 +1,43 @@
 class Solution {
-    public int strStr(String haystack, String needle) {
-        int hLen = haystack.length();
-        int nLen = needle.length();
-        int nIndex = 0;
-        for(int i=0; i<hLen; i++){
-            if(haystack.charAt(i)==needle.charAt(nIndex)){
-                nIndex++;
+    public List<String> fullJustify(String[] words, int maxWidth) {
+        List<String> res = new ArrayList<>();
+        List<String> curWords = new ArrayList<>();
+        int curLen = 0;
+
+        for (String word : words) {
+            if (curLen + word.length() + curWords.size() > maxWidth) {
+                int totalSpaces = maxWidth - curLen;
+                int gaps = curWords.size() - 1;
+                if (gaps == 0) {
+                    res.add(curWords.get(0) + " ".repeat(totalSpaces));
+                } else {
+                    int spacePerGap = totalSpaces / gaps;
+                    int extraSpaces = totalSpaces % gaps;
+                    StringBuilder line = new StringBuilder();
+                    for (int i = 0; i < curWords.size(); i++) {
+                        line.append(curWords.get(i));
+                        if (i < gaps) {
+                            line.append(" ".repeat(spacePerGap));
+                            if (i < extraSpaces) {
+                                line.append(' ');
+                            }
+                        }
+                    }
+                    res.add(line.toString());
+                }
+                curWords.clear();
+                curLen = 0;
             }
-            else{
-                i=i-nIndex;
-                nIndex=0;
-            }
-            if(nIndex==nLen){
-                return i-nLen+1;
-            }
+            curWords.add(word);
+            curLen += word.length();
         }
-        return -1;
+
+        StringBuilder lastLine = new StringBuilder(String.join(" ", curWords));
+        while (lastLine.length() < maxWidth) {
+            lastLine.append(' ');
+        }
+        res.add(lastLine.toString());
+
+        return res;
     }
 }
