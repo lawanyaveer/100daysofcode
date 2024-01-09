582d4f57331b64b758a285a5d6d5806799eeb422
Lawanya Veer <112944303+lawanyaveer@users.noreply.github.com>
2024-01-09 22:12:34 +0530

    Updated Solution.java

---
+++ Day_53/Leetcode/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String simplifyPath(String path) {
+        String[] palabras = path.split("/");
+        List<String> respuesta = new ArrayList<>();
+        for (String palabra : palabras) {
+            if (palabra.equals("..")) {
+                if (!respuesta.isEmpty())  
+                    respuesta.remove(respuesta.size() - 1);
+                continue;
+            }
+            if (!palabra.equals("") && !palabra.equals(".")) {
+                respuesta.add(palabra);
+            }
+        }
+        return "/" + String.join("/", respuesta);
+    }
+}
